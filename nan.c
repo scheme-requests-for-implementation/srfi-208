ec64ac2eb09d3b5b4cd023df4e6d52130aadce97
Wolfgang Corcoran-Mathe <wcm@sigwinch.xyz>
2020-10-27 16:16:14 -0400

    Add license header and delete debug driver.

--- nan.c
+++ nan.c
@@ -1,3 +1,24 @@
+/* Copyright (C) 2020 Wolfgang Corcoran-Mathe
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -56,10 +77,4 @@ uint64_t nan_payload(double nan)
 {
         echknan(nan, "nan_payload");
         return bitsof(nan) & payload_mask;
-}
-
-int main()
-{
-        printf("%lx\n", nan_payload(nan("314159")));
-        return 0;
 }
